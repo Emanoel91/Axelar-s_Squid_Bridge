25fe342ff4143c8f864b31a5c82adbc58701a3b5
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-12 23:30:37 +0330

    Update 2_🚧By_Routes.py

--- pages/2_🚧By_Routes.py
+++ pages/2_🚧By_Routes.py
@@ -1,1 +1,34 @@
+import streamlit as st
+import pandas as pd
+import snowflake.connector
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
+
+# --- Page Config ------------------------------------------------------------------------------------------------------
+st.set_page_config(
+    page_title="Axelar's Squid Bridge",
+    page_icon="https://pbs.twimg.com/profile_images/1938625911743524864/ppNPPF84_400x400.jpg",
+    layout="wide"
+)
+
+# --- Title with Logo -----------------------------------------------------------------------------------------------------
+st.title("🚧By Routes")
+
+st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
+st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
+
+# --- Snowflake Connection ----------------------------------------------------------------------------------------
+conn = snowflake.connector.connect(
+    user=st.secrets["snowflake"]["user"],
+    password=st.secrets["snowflake"]["password"],
+    account=st.secrets["snowflake"]["account"],
+    warehouse="SNOWFLAKE_LEARNING_WH",
+    database="AXELAR",
+    schema="PUBLIC"
+)
+# --- Date Inputs ---------------------------------------------------------------------------------------------------
+timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
+start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("2025-07-31"))
 
